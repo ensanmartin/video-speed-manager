d0a40ae1fb44312aa15891c72932716f8c1ec47f
ensanmartin <4.2.sanmartin.ezequiel@gmail.com>
2024-02-03 02:10:54 -0300

    Update folder structure for better organization

---
+++ js/content-scripts/content-script.js
@@ -0,0 +1,19 @@
+let videoDOM = document.querySelector('video');
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    videoDOM = document.querySelector('video');
+
+    //console.log(videoDOM);
+
+    if (request.speed === "change") {
+        videoDOM.playbackRate = request.newSpeed;
+        sendResponse({currentSpeed: videoDOM.playbackRate});
+
+    } else if (request.video === "exists" && videoDOM !== null) {
+        sendResponse({videoExists: true, lastSpeed: videoDOM.playbackRate});
+
+    } else {
+        sendResponse({videoExists: false, lastSpeed: 1.0});
+        
+    } 
+});
